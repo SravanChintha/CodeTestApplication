5420c56e043ff03a26f11f901f4a971e636d6a17
ImSanjayChintha <sanjaychintha@gmail.com>
2020-07-29 16:08:45 +0530

    First Commit

---
+++ app/src/main/java/com/sravan/codetestapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.sravan.codetestapp;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.text);
+
+    }
+}
